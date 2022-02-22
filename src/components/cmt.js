cbb87a6cc5631bca9f250abbe20e0c5617455ef1
Duc Mai Thanh <ducmtph14090@fpt.edu.vn>
2022-02-22 16:56:20 +0700

    bainew

--- src/components/cmt.js
+++ src/components/cmt.js
@@ -1,7 +1,7 @@
 const CmtPage = {
     render() {
         return /*html*/`
-       
+       <h1>
         <div class="fb-comments"
         data-href="https://developers.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FHasu03%2Fposts%2F630616701342656&show_text=true&width=500"
         data-width="" data-numposts="5">
